6c5fec85496af4ebed94bbe022684bedc531aa3a
Phạm Hoàng Anh Tuấn <33698227+KGRC199913@users.noreply.github.com>
2018-12-09 12:25:44 +0700

    Add files via upload

--- 02.asm
+++ 02.asm
@@ -1,5 +1,5 @@
 .data
-	arr:	.space	16
+	arr:	.space	24
 .text
 j	main
 
@@ -20,19 +20,18 @@ qsort:	# $a0: arr, $a1: left, $a2: right
 	sll	$s3, $s4, 2
 	add	$s3, $s3, $a0
 	lw	$s3, 0($s3)	# pivot = a[mid]
-
 partition_loop:
-	blt	$s1, $s0, recursive	# right > left -> skip while loop
+	blt	$s1, $s0, recursive	# right > left -> skip while loops
 left_loop:
 	add	$t0, $a0, $s0
 	lw	$t1, 0($t0)
-	ble	$s3, $t1, right_loop
+	ble	$s3, $t1, right_loop	# while a[lo] < pivot
 	add	$s0, $s0, 4
 	j	left_loop
 right_loop:
 	add	$t0, $a0, $s1
 	lw	$t1, 0($t0)
-	ble	$t1, $s3, check_and_swap
+	ble	$t1, $s3, check_and_swap # while a[hi] > pivot
 	sub	$s1, $s1, 4
 	j	right_loop
 check_and_swap:
@@ -60,13 +59,17 @@ recursive:
 	sw	$a1, 0($sp)
 	add	$sp, $sp, -4
 	sw	$a2, 0($sp)
+	add	$sp, $sp, -4
+	sw	$s4, 0($sp)
 	move	$a2, $s4
 	jal	qsort		# qsort(arr, left, mid)
+	lw	$s4, 0($sp)
+	add	$sp, $sp, 4
 	lw	$a2, 0($sp)
 	add	$sp, $sp, 4
 	lw	$a1, 0($sp)
 	add	$sp, $sp, 4
-	add	$a1, $s4, 1	# left + 1
+	add	$a1, $s4, 1	# mid + 1
 	jal	qsort		# qsort(arr, mid + 1, right)
 	lw	$ra, 0($sp)
 	add	$sp, $sp, 4
@@ -77,14 +80,19 @@ main:
 	la	$a0, arr
 	li	$t0, 6
 	sw	$t0, 0($a0)
-	li	$t0, 4
+	li	$t0, 5
 	sw	$t0, 4($a0)
-	li	$t0, 2
+	li	$t0, 4
 	sw	$t0, 8($a0)
 	li	$t0, 3
 	sw	$t0, 12($a0)
+	li	$t0, 2
+	sw	$t0, 16($a0)
+	li	$t0, 1
+	sw	$t0, 20($a0)
 	li	$a1, 0
-	li	$a2, 3
+	li	$a2, 5
 	jal	qsort
+exit:
 	li	$v0, 10
 	syscall
